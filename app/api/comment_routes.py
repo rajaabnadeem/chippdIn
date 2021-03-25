151ab20289d6394d606bda0304b620611ebae200
James Aikens <jaikens19@hotmail.com>
2021-03-25 17:08:59 -0500

    Creating comments query

--- app/api/comment_routes.py
+++ app/api/comment_routes.py
@@ -6,15 +6,18 @@ comment_routes =  Blueprint('comments', __name__)
 
 @comment_routes.route('/')
 def getComments(expense_id):
-    comments = Comment.query.join(User).filter(Comment.expense_id == expense_id).all()
+    comments = db.session.query(Comment, User).join(User).filter(Comment.expense_id == expense_id).all() 
+    # Comment.expense_id == expense_id and
     # users = User.query.all()
 
     commentDict = {}
     # print(comments.to_dict())
     for comment in comments:
-        print(comment.to_dict())
-        commentDict[comment.id] = comment.to_dict()
-        commentDict['first_name': comment.first_name]
+        # print('Comment:::', comment.to_dict())
+        print('Comment/User:::', comment.Comment.to_dict())
+       
+        # commentDict[comment.id] = comment.to_dict()
+        # commentDict['first_name': comment.first_name]
         # commentDict['last_name': comment.User.last_name]
 
     return commentDict
