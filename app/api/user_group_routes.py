7885e876bf53a89d70fbd0897785ceecb2e7fc54
dornkholland <dornkholland@gmail.com>
2021-03-27 14:34:36 -0600

    added backend post usergroup route

--- app/api/user_group_routes.py
+++ app/api/user_group_routes.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, jsonify
 from flask_login import login_required
-from app.models import UserGroup, User
+from app.models import UserGroup, User, db
 from app.forms import UserGroupForm 
 
 user_group_routes = Blueprint('user_groups', __name__)
@@ -10,13 +10,10 @@ user_group_routes = Blueprint('user_groups', __name__)
 def add_user_group(group_id):
     form = UserGroupForm()
     usergroup = UserGroup()
-    email = form.email
-    print('================')
-    print(email)
+    email = form.email.data
     user = User.query.filter(User.email == email).one()
-    usergroup.user_id = user_id
+    usergroup.user_id = user.id
     usergroup.group_id = group_id
-    db.session.add(comment)
+    db.session.add(usergroup)
     db.session.commit()
-    user = User.query.filter(User.id == comment.user_id).one()
     return usergroup.to_dict()
