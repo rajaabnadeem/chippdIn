dcabd351e09c9a3e16b8b1f9ef93c96b5631dd55
Stephen Szelpal <stephenszelpal@icloud.com>
2021-03-28 12:21:21 -0400

    merging with add user to group

--- react-app/src/components/grp/Group/UserGroupForm.js
+++ react-app/src/components/grp/Group/UserGroupForm.js
@@ -1,25 +1,26 @@
-import React, { useState } from "react";
-import { useDispatch } from "react-redux";
-import {createUserGroup} from "../../../store/groups"
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { createUserGroup } from '../../../store/groups';
 
-const UserGroupForm = ({group}) => {
-    const dispatch = useDispatch()
-    const [email, setEmail] = useState('')
+const UserGroupForm = ({ group }) => {
+    const dispatch = useDispatch();
+    const [email, setEmail] = useState('');
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-        await dispatch(createUserGroup(
-            group.id,
-            email
-          )
-        );
+        await dispatch(createUserGroup(group.id, email));
     };
 
+    return (
+        <form onSubmit={handleSubmit}>
+            <i class="fas fa-plus"></i>
+            <input
+                placeholder="add group members by email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+            ></input>
+        </form>
+    );
+};
 
-    return(<form onSubmit={handleSubmit}>
-        <input value={email} onChange={(e) => setEmail(e.target.value)}></input>
-        <button>Add user by email</button>
-    </form>)
-}
-
-export default UserGroupForm
+export default UserGroupForm;
--- react-app/src/components/grp/Group/index.js
+++ react-app/src/components/grp/Group/index.js
@@ -1,108 +1,133 @@
-import React, { useState, useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import "./Group.css";
-import Transactions from "../../Transactions";
-import { getTransactions } from "../../../store/transactions";
-import ExpenseForm from "../../exp/ExpenseForm";
-import ExpenseDetails from "../../exp/ExpenseDetails";
-import Modal from "react-modal";
-import { getExpenses, setExpense } from "../../../store/expenses";
-import UserGroupForm from './UserGroupForm'
+import React, { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import './Group.css';
+import Transactions from '../../Transactions';
+import { getTransactions } from '../../../store/transactions';
+import ExpenseForm from '../../exp/ExpenseForm';
+import ExpenseDetails from '../../exp/ExpenseDetails';
+import Modal from 'react-modal';
+import { getExpenses, setExpense } from '../../../store/expenses';
+import UserGroupForm from './UserGroupForm';
+import logo from '../../../images/logo2.jpg';
 
 const Group = ({ group }) => {
-  const dispatch = useDispatch();
-  const [showTransactions, setShowTransactions] = useState(false);
-  const [modalIsOpen, setModalIsOpen] = useState(false);
-  const [expense, setExpense] = useState([]);
-  const user = useSelector((state) => state.session.user);
-  const expenses = useSelector((state) => state.expenses);
-  const transactions = useSelector((state) => state.transactions);
+    const dispatch = useDispatch();
+    const [showTransactions, setShowTransactions] = useState(false);
+    const [modalIsOpen, setModalIsOpen] = useState(false);
+    const [expense, setExpense] = useState([]);
+    const user = useSelector((state) => state.session.user);
+    const expenses = useSelector((state) => state.expenses);
+    const transactions = useSelector((state) => state.transactions);
 
-  Modal.setAppElement(document.getElementById("root"));
+    Modal.setAppElement(document.getElementById('root'));
 
-  const style = {
-    overlay: {
-      textAlign: "center",
-      top: "35px",
-      backgroundColor: "rgba(0,0, 0, 0.1)",
-      zIndex: "1000",
-    },
-  };
+    const style = {
+        overlay: {
+            textAlign: 'center',
+            top: '35px',
+            backgroundColor: 'rgba(0,0, 0, 0.1)',
+            zIndex: '1000',
+        },
+    };
 
-  const toggleTransactions = async () => {
-    await dispatch(getTransactions(userId, group.id));
-    showTransactions ? setShowTransactions(false) : setShowTransactions(true);
-  };
+    const toggleTransactions = async () => {
+        await dispatch(getTransactions(userId, group.id));
+        showTransactions
+            ? setShowTransactions(false)
+            : setShowTransactions(true);
+    };
 
-  let userId;
-  if (user) {
-    userId = user.id;
-  }
+    let userId;
+    if (user) {
+        userId = user.id;
+    }
 
-  useEffect(() => {
-    dispatch(getExpenses(userId, group.id));
-  }, [dispatch]);
+    useEffect(() => {
+        dispatch(getExpenses(userId, group.id));
+    }, [dispatch]);
 
-  const handleExpenses = () => {
-    modalIsOpen ? setModalIsOpen(false) : setModalIsOpen(true);
-  };
+    const handleExpenses = () => {
+        modalIsOpen ? setModalIsOpen(false) : setModalIsOpen(true);
+    };
 
-  const exp = (e) => {
-    Object.entries(expenses).map(([key, value]) => {
-      if (key === e.target.value) {
-        setExpense(value);
-      }
-    });
-  };
+    const exp = (e) => {
+        Object.entries(expenses).map(([key, value]) => {
+            if (key === e.target.value) {
+                setExpense(value);
+            }
+        });
+    };
 
-  if (!showTransactions) {
-    return (
-      <div className="groupContainer">
-        <div className="groupDetails">
-          <img src={group.img_url}></img>
-          <h1>{group.name}</h1>
-          <div className="groupTypeContainer">
-            <p className="groupType">{group.type}</p>
-          </div>
-          <UserGroupForm group={group} />
-        </div>
-        <div className="expenseDetails">
-          <h1>Expense:</h1>
-          <select key={expense} value={expense} onChange={exp}>
-            {Object.entries(expenses).map(([key, value]) =>
-              value.group_id === group.id ? (
-                <option key={key} value={`${key}`}>
-                  {value.description}
-                </option>
-              ) : null
-            )}
-          </select>
-          <div>
-            <button onClick={handleExpenses}>view expense</button>
-          </div>
-          <Modal className="exp-modal" style={style} isOpen={modalIsOpen}>
-            <ExpenseDetails expense={expense} />
-            <button onClick={handleExpenses}>x</button>
-          </Modal>
-          <div>
-            <button onClick={toggleTransactions}>view transaction</button>
-          </div>
-          <div className="create-expense">
-            <ExpenseForm group={group} />
-          </div>
-        </div>
-      </div>
-    );
-  } else {
-    return (
-      <div className="transactionContainer">
-        <Transactions transactions={transactions} group={group} />
-        <div>
-          <button onClick={toggleTransactions}>{"< back"}</button>
-        </div>
-      </div>
-    );
-  }
+    if (!showTransactions) {
+        return (
+            <div className="groupContainer">
+                <div className="groupDetails">
+                    <div>
+                        <img className="groupImage" src={logo}></img>
+                        {/* ^^^ above is a placeholder for group.img_url */}
+                    </div>
+                    <div className="nameTypeContainer">
+                        <div className="groupName">
+                            <h1>{group.name}</h1>
+                        </div>
+                        <div className="groupTypeContainer">
+                            <p className="groupType">{`for your, ${group.type}`}</p>
+                        </div>
+                    </div>
+                    <div className="topRightContainer">
+                        <div className="groupForm">
+                            <UserGroupForm group={group} />
+                        </div>
+                        <div>
+                            <button onClick={toggleTransactions}>
+                                group transactions
+                            </button>
+                        </div>
+                    </div>
+                </div>
+                <div className="expenseDetails">
+                    <div className="expenseLabel">
+                        <h1>Expense:</h1>
+                    </div>
+                    <div className="expenseSelectorContainer">
+                        <select key={expense} value={expense} onChange={exp}>
+                            {Object.entries(expenses).map(([key, value]) =>
+                                value.group_id === group.id ? (
+                                    <option key={key} value={`${key}`}>
+                                        {value.description}
+                                    </option>
+                                ) : null
+                            )}
+                        </select>
+                    </div>
+                    <div className="viewExpenseButton">
+                        <button onClick={handleExpenses}>view expense</button>
+                    </div>
+                    <Modal
+                        className="exp-modal"
+                        style={style}
+                        isOpen={modalIsOpen}
+                    >
+                        <ExpenseDetails expense={expense} />
+                        <button onClick={handleExpenses}>x</button>
+                    </Modal>
+
+                    <div className="create-expense">
+                        <ExpenseForm group={group} />
+                    </div>
+                </div>
+            </div>
+        );
+    } else {
+        return (
+            <div className="transactionContainer">
+                <Transactions transactions={transactions} group={group} />
+                <div>
+                    <button onClick={toggleTransactions}>{'< back'}</button>
+                </div>
+            </div>
+        );
+    }
 };
 
 export default Group;
