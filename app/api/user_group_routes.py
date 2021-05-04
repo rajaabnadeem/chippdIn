1119251bf8dc1ee11e6447b98bb99d7c187be4bd
Stephen Szelpal <stephenszelpal@icloud.com>
2021-05-04 11:26:11 -0400

    route finished

--- app/api/user_group_routes.py
+++ app/api/user_group_routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify, session, request
+from flask import Blueprint, jsonify, session, request, json
 from flask_login import login_required
 from app.models import UserGroup, User, db
 from app.forms import UserGroupForm
@@ -17,3 +17,14 @@ def add_user_group(group_id):
     db.session.add(usergroup)
     db.session.commit()
     return usergroup.to_dict()
+
+
+@user_group_routes.route('/')
+def get_user_groups():
+    data = request.data
+    j = json.loads(data)
+    users_in_group = {}
+    users = UserGroup.query.filter(UserGroup.group_id == j['groupId']).all()
+    for user in users:
+        users_in_group[user.id] = user.to_dict()
+    return users_in_group
