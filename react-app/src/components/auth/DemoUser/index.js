aad99ef607b7574084dc0b55259e096b1a322437
dornkholland <dornkholland@gmail.com>
2021-05-04 13:56:02 -0600

    added demo user component

---
+++ react-app/src/components/auth/DemoUser/index.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { useDispatch } from "react-redux";
+import { login } from "../../../store/session";
+
+const DemoUser = () => {
+  dispatch = useDispatch();
+  const handleDemo = () => {
+    const demoUser = {
+      email: "demo@user.com",
+      password: "password",
+    };
+    dispatch(login(demoUser));
+
+    return;
+  };
+
+  return (
+    <button className="demoUser" onClick={handleDemo}>
+      Demo User
+    </button>
+  );
+};
+
+export default DemoUser;
